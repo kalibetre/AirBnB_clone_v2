be0c6d1a6ea4cd85074feecf014fee402d934f41
kalibetre <kalbetre@icloud.com>
2022-12-07 01:32:24 +0300

    update Place model for sql storage

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@ This Module contains a definition for City Class
 """
 
 from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 from models.base_model import Base, BaseModel
 
@@ -19,3 +20,8 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship(
+        "Place",
+        cascade='all, delete, delete-orphan',
+        backref="cities",
+    )
