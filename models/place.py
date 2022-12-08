28e7afa6c8464fecf5f7937fccdf1b3f2edffadf
Michael Champions <immichaelattah@gmail.com>
2022-12-08 03:21:50 +0300

    update place attributes

--- models/place.py
+++ models/place.py
@@ -36,17 +36,17 @@ class Place(BaseModel, Base):
     """A class that represents a place
 
     Attributes:
-        city_id (str): The City id.
-        user_id (str): The User id.
-        name (str): The name of the place.
-        description (str): The description of the place.
-        number_rooms (int): The number of rooms of the place.
-        number_bathrooms (int): The number of bathrooms of the place.
-        max_guest (int): The maximum number of guests of the place.
-        price_by_night (int): The price by night of the place.
-        latitude (float): The latitude of the place.
-        longitude (float): The longitude of the place.
-        amenity_ids (list): A list of Amenity ids.
+        city_id : The City id.
+        user_id : The User id.
+        name : The name of the place.
+        description : The description of the place.
+        number_rooms : The number of rooms of the place.
+        number_bathrooms : The number of bathrooms of the place.
+        max_guest : The maximum number of guests of the place.
+        price_by_night : The price by night of the place.
+        latitude : The latitude of the place.
+        longitude : The longitude of the place.
+        amenity_ids : A list of Amenity ids.
     """
 
     __tablename__ = "places"
