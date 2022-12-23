eec97dffcb4a78b8749642ed0e2841b2961843c0
kalibetre <kalbetre@icloud.com>
2022-12-24 01:59:39 +0300

    refactor 2-do 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,8 +3,6 @@
 
 A Fabric script that deploys an archive to a web server
 """
-from os import path
-
 from fabric.api import env, put, run
 
 env.hosts = ["54.237.44.150", "100.25.0.186"]
@@ -20,42 +18,20 @@ def do_deploy(archive_path):
         bool: True if success else False
     """
     try:
-        if not path.exists(archive_path):
-            return False
-
-        archive_name_w_ext = path.basename(archive_path)
-        archive_name = archive_name_w_ext.split(".")[0]
-
-        put(archive_path, "/tmp/")
-        run(
-            "mkdir -p mkdir -p /data/web_static/releases/{}/".format(
-                archive_name)
-        )
-
-        run(
-            "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-                archive_name_w_ext, archive_name)
-        )
-
-        run("rm /tmp/{}".format(archive_name_w_ext))
-
-        run(
-            "mv /data/web_static/releases/{}/web_static/*\
-                /data/web_static/releases/{}/".format(
-                archive_name, archive_name)
-        )
-
-        run(
-            "rm -rf /data/web_static/releases/{}/web_static/".format(
-                archive_name)
-        )
-
-        link = "/data/web_static/current"
-        run("rm -rf {}".format(link))
-        run(
-            "ln -s /data/web_static/releases/{}/ {}".format(archive_name, link)
-        )
-        print("New version deployed!")
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}/'.format(
+            archive_path[9:-4]))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            archive_path[9:], archive_path[9:-4]))
+        run('rm /tmp/{}'.format(archive_path[9:]))
+        path = 'mv /data/web_static/releases/{}/web_static/* \
+              /data/web_static/releases/{}/'
+        run(path.format(archive_path[9:-4], archive_path[9:-4]))
+        run('rm -rf  /data/web_static/releases/{}/web_static/'.format(
+            archive_path[9:-4]))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ {}'.format(
+            archive_path[9:-4], '/data/web_static/current'))
         return True
     except Exception:
         return False
