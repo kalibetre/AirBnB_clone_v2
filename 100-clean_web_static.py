9548aa73bca1e95197036933b1495bd5798ca1af
kalibetre <kalbetre@icloud.com>
2022-12-24 02:45:58 +0300

    refactor 100-clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,7 +3,7 @@
 
 A Fabric script has extra functionality that cleans deployments
 """
-from fabric.api import env, local, run
+from fabric.api import cd, env, lcd, local, run
 
 env.hosts = ["54.237.44.150", "100.25.0.186"]
 
@@ -15,18 +15,13 @@ def do_clean(number=0):
         number (int, optional): number of versions to keep. Defaults to 0.
     """
     number = int(number)
-    number = number if number >= 2 else 1
+    number = (number if number >= 2 else 1) + 1
 
     try:
-        local(
-            "ls -t /versions | tail -n +{} | xargs rm -rf".format(number + 1)
-        )
+        with lcd("versions"):
+            local("ls -1t | tail -n +{} | xargs rm -rf".format(number))
 
-        remote_path = "/data/web_static/releases"
-        run(
-            "ls -t {} | tail -n +{} | xargs rm -rf".format(
-                remote_path, number + 1
-            )
-        )
+        with cd("/data/web_static/releases"):
+            run("ls -1t | tail -n +{} | xargs rm -rf".format(number))
     except Exception:
         pass
