8691dd9649cc21d0b5fcb75ffe21443ba3a3ec4d
kalibetre <kalbetre@icloud.com>
2022-12-24 02:11:40 +0300

    refactor 2-do original

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,6 +3,8 @@
 
 A Fabric script that deploys an archive to a web server
 """
+from os import path
+
 from fabric.api import env, put, run
 
 env.hosts = ["54.237.44.150", "100.25.0.186"]
@@ -18,20 +20,23 @@ def do_deploy(archive_path):
         bool: True if success else False
     """
     try:
-        put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}/'.format(
-            archive_path[9:-4]))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            archive_path[9:], archive_path[9:-4]))
-        run('rm /tmp/{}'.format(archive_path[9:]))
-        path = 'mv /data/web_static/releases/{}/web_static/* \
-              /data/web_static/releases/{}/'
-        run(path.format(archive_path[9:-4], archive_path[9:-4]))
-        run('rm -rf  /data/web_static/releases/{}/web_static/'.format(
-            archive_path[9:-4]))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ {}'.format(
-            archive_path[9:-4], '/data/web_static/current'))
+        archive_name_w_ext = path.basename(archive_path)
+        archive_name = archive_name_w_ext.split(".")[0]
+
+        rel_path = "/data/web_static/releases/{}/".format(archive_name)
+        tmp_path = "/tmp/{}".format(archive_name_w_ext)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(rel_path))
+        run("tar -xzf {} -C {}".format(tmp_path, rel_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(rel_path, rel_path))
+        run("rm -rf {}web_static/".format(rel_path))
+
+        link = "/data/web_static/current"
+        run("rm -rf {}".format(link))
+        run("ln -s {} {}".format(rel_path, link))
+        print("New version deployed!")
         return True
     except Exception:
         return False
