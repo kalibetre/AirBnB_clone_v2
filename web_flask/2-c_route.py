a9ff459a6c08fbc2ba9cc11e91860df111f641eb
kalibetre <kalbetre@icloud.com>
2023-01-14 21:05:27 +0300

    fix _ replacement with space

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -38,7 +38,7 @@ def c_text(text):
     Returns:
         str: c followed by the given text
     """
-    return "C {}".format(text.replace("_", ""))
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
