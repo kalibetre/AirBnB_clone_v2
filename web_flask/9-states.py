1e0dcdfccf4ff60f44f60aabf76d3a4f823b07d9
kalibetre <kalbetre@icloud.com>
2023-01-17 19:03:54 +0300

    add flask app that handles states api call

---
+++ web_flask/9-states.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""A Flask web application
+
+supported api end points
+  - /states : returns list of the states
+  - /states/id : returns a state with list of cities
+"""
+from flask import Flask, render_template
+
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Cleanup Db Session
+    """
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """Lists states
+
+    Returns:
+        html: list of states in template format
+    """
+    states = list(storage.all(State).values())
+    return render_template("9-states.html", display="multiple", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def state(id):
+    """Returns a state information
+
+    Args:
+        id (int): the id of the state
+
+    Returns:
+        html: state information n a template format
+    """
+    states = list(storage.all(State).values())
+    result = list(filter(lambda x: x.id == id, states))
+    state = result[0] if len(result) > 0 else False
+    return render_template("9-states.html", display="single", state=state)
+
+
+if __name__ == "__main__":
+    storage.reload()
+    app.run()
