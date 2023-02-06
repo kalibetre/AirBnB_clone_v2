040190067711d7cd031185b935a9f1e68e5d4db8
kalibetre <kalbetre@icloud.com>
2023-02-06 09:34:48 +0300

    change route of 0-hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello():
     """A simple root route
 
@@ -19,4 +19,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run("0.0.0.0", 5001)
